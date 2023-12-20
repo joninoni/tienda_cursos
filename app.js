8b61b67b0ab2627a34afa3c9f92ceaf07884d05d
joninoni <jonibeltranhayer@gmail.com>
2023-12-20 15:36:26 -0600

    funcion que lee el curso seleccionado y funcion que crea un objecto del curso

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+const lista =document.querySelector("#lista");
+let arreglo=[];
 
+//eventos
+lista.addEventListener("click",agregarCurso);
+
+
+//funciones
+function agregarCurso(e){
+    e.preventDefault();
+    if(e.target.classList.contains("curso__carrito")){
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerCurso(cursoSeleccionado);
+    }
+}
+function leerCurso(curso){
+    const objCurso={
+        titulo:curso.querySelector("h2").textContent,
+        img:curso.querySelector("img").src,
+        id:curso.querySelector("a").getAttribute("data-id"),
+        precio:curso.querySelector(".curso__precio").textContent,
+    }
+    arreglo=[...arreglo,objCurso];
+}
