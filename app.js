2c92df98b1f477b81bc69054b1e00126a398b3ba
joninoni <jonibeltranhayer@gmail.com>
2023-12-21 21:34:01 -0600

    funcion que actualiza la cantidad y funcion que elimina los cursos

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const lista =document.querySelector("#lista");
 const tbody= document.querySelector("#tbody");
+const tabla =document.querySelector(".tabla");
 let arreglo=[];
 
 //eventos
-lista.addEventListener("click",agregarCurso);
-
+listaEventos();
+function listaEventos(){
+    lista.addEventListener("click",agregarCurso);
+    tabla.addEventListener("click",eliminarCurso);
+}
 
 //funciones
+function eliminarCurso(e){
+    e.preventDefault();
+    if(e.target.classList.contains("borrar-curso")){
+       const id=e.target.getAttribute("data-id");
+       arreglo=arreglo.filter( curso => curso.id !==id)
+       crearHtml()
+    }
+}
 function agregarCurso(e){
     e.preventDefault();
     if(e.target.classList.contains("curso__carrito")){
@@ -22,7 +34,23 @@ function leerCurso(curso){
         precio:curso.querySelector(".curso__precio").textContent,
         cantidad:1,
     }
-    arreglo=[...arreglo,objCurso];
+    const existe = arreglo.some( curso => curso.id === objCurso.id);
+    if(existe){
+        const cursos = arreglo.map( curso => {
+            if(curso.id === objCurso.id){
+                curso.cantidad++
+                return curso
+            }
+            else{
+                return curso
+            }
+        })
+        arreglo=[...cursos];
+    }
+    else{
+        arreglo=[...arreglo,objCurso];
+    }
+    console.log(arreglo);
     crearHtml();
 }
 
@@ -36,6 +64,7 @@ function crearHtml(){
                       <td> ${curso.titulo}</td>
                       <td> ${curso.precio}</td>
                       <td> ${curso.cantidad}</td>
+                      <td> <a href="#" class="borrar-curso" data-id=${curso.id}>X</a> </td>
                       `
         tbody.appendChild(tr);
     })
